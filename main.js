baa462886423c65edaa9b6f320149c7f790ef111
Dominik Duda <dominik.duda6@gmail.com>
2020-05-27 09:53:10 +0200

    lol

---
+++ main.js
@@ -0,0 +1,78 @@
+/* BASIC 1
+Write a function named crystalGazer() that takes four arguments: number of children, partner's 
+name, geographic
+ location and job title.
+ Once the user passes the four arguments the program should output the following message on screen:
+
+You will be a job_title in location and married to partners_name with number_childrens.
+
+function crystalGazer(number_of_children, partners_name, location, job_title) {
+    document.write("You will be a " + job_title + " in " + location + " and married to " + partners_name + " with " + number_of_children + "children <br>")
+
+}
+crystalGazer(100, "Frau Holle&Snoopy", "Buxdehude", "Anschaffer")
+*/
+document.write("j")
+    /*
+    Basic 2 | Age Calculator
+    Write a function called ageCalculator() that takes two arguments birth year and current year. 
+    Calculate 2 possible ages based on those
+     years (two, because we don't know the birth month).
+
+    You should get the following results:
+
+    You are either 29 or 28 */
+
+
+
+
+/*
+Basic 3 | Age Calculator-improved
+Enhance your previous exercise in terms of how to get the current year in JavaScript instead of
+ passing it in. 
+Provide the same results just 
+through passing one argument.
+
+You should get the following results:
+
+You are either 29 or 28*/
+
+/*
+Basic 4 | Degree-Radian Conversion
+Create a JavaScript function that converts degrees to radians. The function accepts one argument,
+ the degrees,
+ and outputs the radians based on
+ them. The formula for converting degrees to radians is: "degrees * (pi/180)"
+
+E.g. If user passes 90 degrees it should get the result of 1.57079 radians
+
+*/
+
+
+
+/*
+Basic 5 | Area and Volume of a box
+Create a function that calculates the area and volume of a box.
+
+Formulas:
+
+area = width x height
+
+volume = width x height x depth
+
+Pass three different numbers as arguments and get calculated results using the return statement.
+
+You should get the following results:
+
+The area of the box is: 14
+
+The volume of the box is: 70*/
+
+/*
+
+Basic 6 | Product
+Create a JavaScript function to calculate the product of the values in an array. You can have 
+as many values in an array as you want.
+
+E.g. If we pass the following values 1, 3, 7, 10, 2 we should get the number 420 printed on the
+ screen.*/
